91266649996adf0b15babe0ba218da4faf742bed
SuhEugene <suheugene@ya.ru>
2024-08-11 21:05:19 +0300

    Move from static to docs

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.listen(8080, () => console.log("Listening http://localhost:8080"))
